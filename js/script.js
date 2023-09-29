b5a78d39852c1190df4431fd8f78c84902b790bb
localbu <143374337+localbu@users.noreply.github.com>
2023-09-29 14:37:53 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//scrool reveal
+ScrollReveal({
+  reset: true,
+  distance:'500px',
+  duration:1000,
+  delay:200
+});
+
+ScrollReveal().reveal('.nav, .pamflet', { origin: 'top' });
+ScrollReveal().reveal('.recomended, .phone', { origin: 'left' });
+ScrollReveal().reveal('.contain, .watches-pamflet', { origin: 'right' });
+ScrollReveal().reveal('.container-buds', { origin: 'top' });
+
+
